1899f1ddc86af13bdfb4d188ddc3a0dc1d6f9e17
IzaanAnwar <mdizaan67@gmail.com>
2024-08-07 18:32:34 +0530

    build: add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactQueryProvider } from "@/services/providers";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Toaster
+        duration={2000}
+        position="bottom-center"
+        toastOptions={{
+          classNames: {
+            error: "bg-red-500 text-white border-red-700",
+            warning: "bg-yellow-500 text-white border-yellow-700",
+            success: "bg-green-500 text-white border-green-700",
+          },
+        }}
+      />
       <body className={inter.className}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
